c3bcb9ed630e7bff829790b596811de31b6fec93
Lee <la59caddy@gmail.com>
2014-06-14 17:21:45 -0500

    First commit

---
+++ ranch.js
@@ -0,0 +1,27 @@
+function Stallions(name,color,sire,damSire,fee){
+    this.name=name;
+    this.color=color;
+    this.sire=sire;
+    this.damSire=damSire;
+    this.fee=fee;
+}
+Stallions.prototype = {
+  info : function() {
+      console.log(this.name + ": a  " + this.color + " horse by " + this.sire + " out of a mare by " + this.damSire + ". His fee is " + this.fee + "<br />");
+      }
+}
+var stallions=[];
+stallions[0]= new Stallions("Fire Cat","black","Savage Kitten","Savage Kitten"," $100,000 ");
+stallions[1] = new Stallions( "Storm Warning", "bay","Storm Cat","Savage Kitten","$100,000");
+stallions[2]= new Stallions("Royal Flush","chestnut","Affirmed","Seattle Slew", "$100,000");
+stallions[3] = new Stallions(" Northern Ice "," grey "," Holy Bull "," Danzig ", " $25,000 ");
+
+ 
+function stallionBarn(arr) {
+  for(var i = 0; i < arr.length; i++) {
+   arr[i].info();
+  }    
+
+}
+
+stallionBarn(stallions);
